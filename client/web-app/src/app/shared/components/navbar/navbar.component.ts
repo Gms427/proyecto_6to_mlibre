8399ed364197ec62869352eb2027523237997ad6
Ale <alejandroalbarenga30@gmail.com>
2019-05-29 23:48:18 -0300

    listo los favoritos, los elimino y alerto de cada uno y muestro en consola todos.

--- client/web-app/src/app/shared/components/navbar/navbar.component.ts
+++ client/web-app/src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Favorites } from "./favorites";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  favoritesList: Favorites[] = [
+    {
+      id: 1,
+      name: "Chevrolet Celta 1.0 full",
+      price: "U$S 7.450",
+      img: "https://mlu-s1-p.mlstatic.com/700820-MLU29200013016_012019-T.jpg"
+    },
+    {
+      id: 2,
+      name: "Chevrolet Celta 1.0 full",
+      price: "U$S 7.450",
+      img: "https://mlu-s1-p.mlstatic.com/700820-MLU29200013016_012019-T.jpg"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  navegateToProduct(favorite) {
+    alert("Product favorite: " + favorite.name);
   }
 
+  deleteFavorite(favorite) {
+    alert("Delete favorite:" + favorite.name);
+    const index: number = this.favoritesList.indexOf(favorite);
+    if (index !== -1) {
+      this.favoritesList.splice(index, 1);
+    }
+  }
+  listAllFavorites() {
+    console.log(this.favoritesList);
+  }
 }
