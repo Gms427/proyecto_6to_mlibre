a43809b9010f2ce4b47b9b59badff6166751fb0f
Gonzalo Manzzi <gonzalo427@hotmail.com>
2019-06-24 00:19:20 -0300

    Conexión de ejemplo a una db Postgresql

--- server/src/controllers/indexController.ts
+++ server/src/controllers/indexController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express';
 import path from 'path';
+import client from '../database';
 
 class IndexController {
     
+    test(req: Request, res: Response){
+        const table = "customers";
+        const condition = "id = 2"
+        let query = `SELECT * FROM ${table}
+                    WHERE ${condition}`;
+
+        client.query(query, (err: any, response: any) => {
+            console.log(err, res);
+            res.send(response);
+        });
+    }
+
     index(req: Request, res: Response){
         res.send('Hello');
     }
