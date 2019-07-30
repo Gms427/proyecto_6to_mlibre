6ce4c5d8923b595c7af29fe4c45775d4b85e6cd2
Gonzalo Manzzi <gonzalo427@hotmail.com>
2019-07-29 22:07:22 -0300

    Dejo funcionando el componente del historial

--- client/web-app/src/app/user/user.module.ts
+++ client/web-app/src/app/user/user.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { UserHistoryComponent } from './components/user-history/user-history.component';
+import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [UserHistoryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    UserRoutingModule,
+    SharedModule
   ]
 })
 export class UserModule { }
