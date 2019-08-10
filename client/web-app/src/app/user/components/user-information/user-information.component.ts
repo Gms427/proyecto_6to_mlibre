22174616bb527abb9744bd36592cc4a28883d8fd
Ale <alejandroalbarenga30@gmail.com>
2019-08-09 22:18:11 -0300

    Agregue checkbox para editar el campo
    cuando lo checkeas se habilita y se desabilita

--- client/web-app/src/app/user/components/user-information/user-information.component.ts
+++ client/web-app/src/app/user/components/user-information/user-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { TestService } from '../../../shared/services/test.service';
 import { LoginService } from 'src/app/shared/services/login.service';
 import { UserInfo } from 'src/app/shared/utils/types';
@@ -12,12 +13,22 @@ import { UserInfo } from 'src/app/shared/utils/types';
 export class UserInformationComponent implements OnInit {
 
   public user: any;
+  public stateEmail: boolean = false;
+  public stateFullName: boolean = false;
+  public statePhone: boolean = false;
+  public stateDateOfBirth: boolean = false;
+  public stateCity: boolean = false;
+  public stateNeighborhood: boolean = false;
+  public stateDepartament: boolean = false;
+  public stateStreet: boolean = false;
+
 
   constructor(private _TestService: TestService,
     private _loginService: LoginService) { }
 
   ngOnInit() {
     console.log(this.getInformation());
+    // console.log(this.stateEmail.disable)
   }
 
   getInformation() {
