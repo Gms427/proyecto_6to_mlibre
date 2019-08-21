98f4bba50a23e004a6fd5d91d68178d8ac618883
Ale <alejandroalbarenga30@gmail.com>
2019-08-20 23:02:48 -0300

    Arreglos esteticos
    - puse el sidenav mas lindo, y cuando esta oculto que mida 1%
    - cree un componente para el Customer Support, "chat"
    -Le puse los iconos a las categorias cuando vas a publicar
    -estuve cambiando colores y tipo de letra

--- client/web-app/src/app/shared/components/navbar/navbar.component.ts
+++ client/web-app/src/app/shared/components/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { TranslateService } from "@ngx-translate/core";
 import { LoginService } from "../../services/login.service";
 import { ActivatedRoute, Router, NavigationEnd } from "@angular/router";
 import { NavbarService } from "src/app/shared/services/navbar.service";
-import { MatSidenav } from '@angular/material';
+import { MatSidenav, MatBottomSheet } from '@angular/material';
+import { HelpComponent } from '../help/help.component';
 
 @Component({
   selector: "app-navbar",
@@ -16,13 +17,15 @@ export class NavbarComponent implements OnInit {
   public navbarColor: string;
   public flag: boolean;
   public searchNadvar: boolean;
-  public width = 6;
+  public width = 1;
+  
 
   @ViewChild("sidenav") sideNav: MatSidenav;
 
   constructor(
     public _loginService: LoginService,
-    public _navbarService: NavbarService
+    public _navbarService: NavbarService,
+    private _bottomSheet: MatBottomSheet
   ) {}
 
   ngOnInit() {
@@ -44,14 +47,17 @@ export class NavbarComponent implements OnInit {
   }
 
   test(){
-    console.log('toggle sidenav');
     this.width = 300;
     this.sideNav.toggle();
   }
 
+  openBottomSheet(): void {
+    this._bottomSheet.open(HelpComponent);
+  }
+
   close(){
     setTimeout(() => {
-      this.width = 6;
+      this.width = 1;
     }, 300);
   }
 }
